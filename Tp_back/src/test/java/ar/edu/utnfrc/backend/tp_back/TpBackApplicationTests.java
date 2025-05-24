d75abe59f1e87f20a13a2615460221f93d7b2e5d
Marcos Ignacio Belli <95348@sistemas.frc.utn.edu.ar>
2025-05-23 22:11:00 -0300

    hola

---
+++ Tp_back/src/test/java/ar/edu/utnfrc/backend/tp_back/TpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utnfrc.backend.tp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
